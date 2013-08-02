af402fde8785813b83c8bbc9d7ecbfd0c5e43fad
polat.aydin <polattt@gmail.com>
2013-08-02 16:27:07 +0300

    HelloWorld

--- js/library.js
+++ js/library.js
@@ -1,7 +1,7 @@
  var SILLY = (function (module) {
 
      module.DoIt = function(resultobject){
-		resultobject.prepend('Fuck me <br/>')
+		resultobject.prepend('Huhu <br/>')
 	 };
 
 	return module;
