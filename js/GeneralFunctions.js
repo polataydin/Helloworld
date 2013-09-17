c9b6bbb3b3a5d85b958f3ccabd27afb351d40ce0
polat.aydin <polattt@gmail.com>
2013-09-17 16:20:29 +0300

    Helloworld

---
+++ js/GeneralFunctions.js
@@ -0,0 +1,97 @@
+﻿function GetCrmData(url,ulid,addhrefclassforclick,localstoragename) {
+
+    var lookuparray = [];
+    var integer = 0;
+    var count = null;
+
+    if (localStorage[localstoragename] == null) {
+
+        $("#" + ulid).empty();
+        while (true) {
+
+            $.ajax({
+
+                type: "GET",
+                contentType: "application/json;charset=utf-8",
+                datatype: "json",
+                url: url,
+                async: false,
+                data: { page: integer },
+                cache: false,
+                processData: true,
+                beforeSend: function (XMLHttpRequest) {
+                    XMLHttpRequest.setRequestHeader("Accept", "application/json");
+                },
+                success: function (data) {//On Successfull service call   
+                    integer = parseInt(integer) + 1;
+                    count = data.d.length;
+                    if (data.d.length > 0) {
+                        for (var i = 0; i < data.d.length; i++) {
+                            // Enhance new listview element      
+
+                            var $li = $('<li />').appendTo($("#"+ulid));
+
+                            var $a = $('<a />').appendTo($li);
+                            $a.attr("href", "#");
+                            $a.addClass(addhrefclassforclick);
+                            $a.text(data.d[i].Name);
+                            $("#" + ulid).listview('refresh');
+
+                            //lookupobject must Name And Guid Standart
+                            var lookup = {};
+                            lookup.Name = data.d[i].Name;
+                            lookup.Guid = data.d[i].Guid;
+                            //we fill the array after we want to create record , we find the selected item guid into this array 
+                            lookuparray.push(lookup);
+                        }
+                    }
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+
+                }
+            });
+            if (count == 0 || count == null) {
+                localStorage[localstoragename] = JSON.stringify(lookuparray);
+                break;
+
+            }
+        }
+
+    }
+    else {
+
+        var storeditems = JSON.parse( localStorage[localstoragename]);
+        $("#"+ulid).empty();
+        for (var i = 0; i < storeditems.length; i++) {
+
+            var $li = $('<li />').appendTo($("#" + ulid));
+
+            var $a = $('<a />').appendTo($li);
+            $a.attr("href", "#");
+            $a.addClass(addhrefclassforclick);
+            $a.text(storeditems[i].Name);
+            $('#' + ulid).listview('refresh');
+        }
+    }
+
+    return localStorage[localstoragename];
+
+}
+
+function dateToWcf(input) {
+    var d = new Date(input);
+    if (isNaN(d)) return null;
+    return '\/Date(' + d.getTime() + '-0000)\/';
+}
+
+function getUrlVars(url) {
+    var vars = [], hash;
+    var hashes = url.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
---
+++ js/PageScripts/MainPage.js
@@ -0,0 +1,100 @@
+﻿var equipments, parkings = [];
+
+$(document).bind("pageinit", function (event) {
+    alert("ok");
+    $("#citylookup").hide();
+    $("#parkinglookup").css("visibility", "hidden");
+
+    $('#citytext').click(function (e) {
+
+        $("#citylookup").click();
+
+        e.stopImmediatePropagation();
+    });
+    $('#parkingtext').click(function (e) {
+
+        $("#parkinglookup").click();
+        e.stopImmediatePropagation();
+    });
+
+    $('#save').click(function (e) {
+        debugger;
+
+        arr = jQuery.grep(parkings, function (n) {
+            return (n.Name = $("#parkingtext").val());
+        });
+        res = jQuery.grep(equipments, function (n) {
+            return (n.Name = $("#citytext").val());
+        });
+
+        //lookups
+        var rezervationslookups = {
+
+            trd_contactid: {
+                Id: "B90A7E10-321A-E311-8BC6-00155D0B0611",
+                ş: "contact"
+            }
+
+        };
+        //optionsets
+        var rezervationsoptionsets = {
+
+            trd_reservationtype: {
+                Value: 167440000
+            }
+
+        };
+        //CrmFields
+        var rezervations = {
+
+            ScheduledStart: dateToWcf($("#startdate").val().replace(/(\d{2})\.(\d{2})\.(\d{4})/, '$3-$2-$1')),
+            ScheduledEnd: dateToWcf($("#enddate").val().replace(/(\d{2})\.(\d{2})\.(\d{4})/, '$3-$2-$1'))
+        };
+
+        $.support.cors = true;
+        $.ajax({
+
+            type: "GET",
+            contentType: "application/json;charset=utf-8",
+            datatype: "json",
+            url: "http://212.109.96.121:5556/Mobile/MobilizmService.svc/CreateRezervation",
+            async: false,
+            data: { rezervations: JSON.stringify(rezervations), rezervationslookups: JSON.stringify(rezervationslookups), rezervationsoptionsets: JSON.stringify(rezervationsoptionsets), origin: 2, ParkingId: res[0].Guid },
+            cache: false,
+            processData: true,
+            beforeSend: function (XMLHttpRequest) {
+                XMLHttpRequest.setRequestHeader("Accept", "application/json");
+            },
+            success: function (data) {//On Successfull service call      
+                alert("Kaydınız alınmıştır");
+                e.stopImmediatePropagation();
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("işlem yapılırken hata oluştu");
+                e.stopImmediatePropagation();
+            }
+        });
+
+
+    });
+});
+$(document).delegate('#dialog1', 'pagebeforeshow', function (event) {
+    debugger;
+    var obj = GetCrmData("http://212.109.96.121:5556/Mobile/MobilizmService.svc/GetEquipments", "citylist", "clickhref", "cities");
+    equipments = JSON.parse(obj);
+    $('.clickhref').click(function () {
+        $("#citytext").val($(this)[0].innerText);
+        $("#dialog1").dialog("close");
+    });
+});
+$(document).delegate('#dialog2', 'pagebeforeshow', function (event) {
+    debugger;
+    var obj = GetCrmData("http://212.109.96.121:5556/Mobile/MobilizmService.svc/GetParkingPoints", "parkinglist", "clickhrefparking", "parkings");
+    parkings = JSON.parse(obj);
+    $('.clickhrefparking').click(function () {
+
+
+        $("#parkingtext").val($(this)[0].innerText);
+        $("#dialog2").dialog("close");
+    });
+});
